75c8c979e9d15127623bd7ffbb06ffec240ecdae
PATEL KRISH <127637917+PATEL-KRISH-0@users.noreply.github.com>
2023-11-16 21:43:42 +0530

    added some code

--- MorseCode Traning/V3 (working)/Source Code/app.js
+++ MorseCode Traning/V3 (working)/Source Code/app.js
@@ -34,6 +34,7 @@ let spacebarPressStartTime = 0;
 let spacebarPressDuration = 0;
 let correct = 0;
 let wrong = 0;
+let tttt = 0;
 
 const letterKeys = Object.keys(morseCode);
 
@@ -66,10 +67,6 @@ function wordMor(word) {
     return arr.join(" ")
 }
 
-// let w = wordMor("krish");
-// console.log(w);
-
-
 const randomCharGenerator = () => {
     const randomLetter = letterKeys[Math.floor(Math.random() * letterKeys.length)];
     return randomLetter
@@ -94,7 +91,7 @@ const reset = () => {
     realAnsOfAns.style.display = "none"
     document.querySelectorAll(".spens-inputs")[1].style.display = "flex"
 
-
+    //! code for chrecters
     // char.innerHTML = randomCharGenerator()
     // input.value = ""
     // output.innerHTML = ""
@@ -106,32 +103,67 @@ const reset = () => {
     // document.querySelectorAll(".spens-inputs")[1].style.display = "flex"
 }
 
-const chackCode = (val = 0) => {
-    if (val == 4) {
-        if (wo[1] == output.innerHTML) {
-            alert("crrect   " + wo[1] + "   =   " + char.innerHTML);
-            reset()
-            correct = correct + 1;
-            R.innerHTML = correct
-            return 0
-        }
-        else {
-            alert(`you are wrong : ${wo[0] + "   =   " + wo[1]}`);
-            alert("try again");
-            output.innerHTML = ""
-            wrong = wrong + 1;
-            W.innerHTML = wrong
-            return 0
-        }
+// const chackCode = (val = 0) => {
+//     // if (val == 4) {
+//         if (wo[1] == output.innerHTML) {
+//             alert("crrect   " + wo[1] + "   =   " + char.innerHTML);
+//             reset()
+//             correct = correct + 1;
+//             R.innerHTML = correct
+//             return 0
+//         }
+//         else {
+//             alert(`you are wrong : ${wo[0] + "   =   " + wo[1]}`);
+//             alert("try again");
+//             output.innerHTML = ""
+//             wrong = wrong + 1;
+//             W.innerHTML = wrong
+//             return 0
+//         }
 
-    }
-    if (morseCode[char.innerHTML] == output.innerHTML || morseCode[char.innerHTML] == input.value) {
-        alert("crrect   " + morseCode[char.innerHTML] + "   =   " + char.innerHTML);
-        reset();
+//     // }
+//     if (morseCode[char.innerHTML] == output.innerHTML || morseCode[char.innerHTML] == input.value) {
+//         alert("crrect   " + morseCode[char.innerHTML] + "   =   " + char.innerHTML);
+//         reset();
+//         correct = correct + 1;
+//         R.innerHTML = correct
+//         return 0;
+//     }
+
+// }
+
+const chackCode = (val = 0) => {
+    // if (val == 4) {
+    // console.log(wo[1] <= (output.innerHTML).length);
+    if (wo[1] == output.innerHTML) {
+        alert("crrect   " + wo[1] + "   =   " + char.innerHTML);
+        reset()
         correct = correct + 1;
         R.innerHTML = correct
-        return 0;
+        return 0
     }
+    else if (wo[1].length <= (output.innerHTML).length) {
+        alert("no not correct   " + wo[1] + "   =   " + wo[0]);
+        reset()
+    }
+
+    // else {
+    //     alert(`you are wrong : ${wo[0] + "   =   " + wo[1]}`);
+    //     alert("try again");
+    //     output.innerHTML = ""
+    //     wrong = wrong + 1;
+    //     W.innerHTML = wrong
+    //     return 0
+    // }
+
+    // }
+    // if (morseCode[char.innerHTML] == output.innerHTML || morseCode[char.innerHTML] == input.value) {
+    //     alert("crrect   " + morseCode[char.innerHTML] + "   =   " + char.innerHTML);
+    //     reset();
+    //     correct = correct + 1;
+    //     R.innerHTML = correct
+    //     return 0;
+    // }
 
 }
 
@@ -157,20 +189,21 @@ const chackCode = (val = 0) => {
 //     }
 
 // }
+
 const chackCodeForInput = () => {
     if (input.value == "") {
         alert("Please Enter Ans");
         return 0;
     }
-    else if (morseCode[char.innerHTML] == input.value) {
-        alert("crrect   " + morseCode[char.innerHTML] + "   =   " + char.innerHTML);
+    else if (wo[1] == input.value) {
+        alert("crrect   " + wo[0] + "   =   " + wo[1]);
         reset()
         correct = correct + 1;
         R.innerHTML = correct;
         return 0;
     }
     else {
-        alert(`you are wrong : ${char.innerHTML + "   =   " + morseCode[char.innerHTML]}`);
+        alert(`you are wrong : ${char.innerHTML + "   =   " + wo[1]}`);
         alert("try again");
         input.value = "";
         wrong = wrong + 1;
@@ -194,14 +227,17 @@ document.addEventListener("keyup", function (event) {
 
         if (spacebarPressDuration < 150) {
             output.innerHTML = output.innerHTML + "."
-            chackCode(output.innerHTML.length)
-
+            chackCode()
+            // chackCode(output.innerHTML.length)
         }
         else {
             output.innerHTML = output.innerHTML + "-"
-            chackCode(output.innerHTML.length)
+            chackCode()
+            // chackCode(output.innerHTML.length)
         }
+        tttt = Date.now()
     }
+
 });
 
 input.addEventListener("input", () => {
@@ -217,15 +253,15 @@ input.addEventListener("input", () => {
 
 hintA.addEventListener("click", () => {
     realAnsOfHint.style.display = "flex"
-    realAnsOfHint.innerHTML = `The length of code is : ${morseCode[char.innerHTML].length}`
+    realAnsOfHint.innerHTML = `The length of code is : ${wo[1].length} (including spece)`
     document.querySelectorAll(".spens-inputs")[0].style.display = "none"
 })
 
 ansA.addEventListener("click", () => {
     realAnsOfAns.style.display = "flex"
-    realAnsOfAns.innerHTML = `Code is : ${morseCode[char.innerHTML]}`
+    realAnsOfAns.innerHTML = `Code is : ${wo[1]}`
+    console.log(wo[1]);
     document.querySelectorAll(".spens-inputs")[1].style.display = "none"
-
 })
 
 submit.onclick = function () {
